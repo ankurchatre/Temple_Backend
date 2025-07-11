035a399d8fa9b5d8047886db4cbd887f565e6d31
ankurchatre <ankurchatre1@gmail.com>
2025-07-11 11:42:42 +0530

    Update WebConfig.java

--- src/main/java/com/main/config/WebConfig.java
+++ src/main/java/com/main/config/WebConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.nio.file.Paths;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
@@ -13,6 +11,4 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:./uploads/");
     }
-}
-
 }
